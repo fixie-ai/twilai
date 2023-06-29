8814be61f15706d1b39e8b1c7483c6e3fcf060a1
juberti <justin@fixie.ai>
2023-06-29 12:22:54 -0700

    Revert "Update route.tsx"
    This reverts commit 7e54e5392ac39b3d92049198b8586a28a587790c.

--- app/api/twiml/route.tsx
+++ app/api/twiml/route.tsx
@@ -86,7 +86,6 @@ function ChatAgent({ animal, conversation }: { animal: string, conversation: str
 
 
 export async function POST(request: NextRequest) {
-  console.log(request);
   const json = await request.json();  
   console.log(json);  
   const input = json.SpeechResult
