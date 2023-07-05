27dbbc12e1c8b09b373d7a096efe0ec726a9daa0
juberti <justin@fixie.ai>
2023-07-05 12:12:49 -0700

    Add test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Index() {
-  return <h1>Twilai Test Page</h1>
+  return (
+    <>
+      <h1>Welcome to the Twilai Test Page!</h1>
+      <p><a href="tel:+14256006002">Click to start a phone call with a friendly AI-generated animal.</a></p>
+    </>
+  );
 }
