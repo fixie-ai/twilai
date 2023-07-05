9db60c6b8d5335e94300b858c4d6e860cc10c5f4
juberti <justin@fixie.ai>
2023-07-05 12:21:05 -0700

    Clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <>
-      <h1>Welcome to the Twilai Test Page!</h1>
-      <p><a href="tel:+14256006002">Click to start a phone call with a friendly AI-generated animal.</a></p>
+      <h1>📞 Welcome to the <a href="https://ai-jsx.com">AI.JSX</a> Voice Agent Demo!</h1>
+      <p>Click to <a href="tel:+14256006002">call 425-600-6002</a> and have a voice chat with a friendly AI-generated animal, powered by AI.JSX and Twilio.</p>
     </>
   );
 }
